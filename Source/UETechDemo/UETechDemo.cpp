74a285cc29908ba91032e0c0f2c01326365b9a26
Ryan LeBlanc <ryanleb2003@gmail.com>
2025-03-18 17:58:43 -0300

    Initial Commit

---
+++ Source/UETechDemo/UETechDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UETechDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UETechDemo, "UETechDemo" );
